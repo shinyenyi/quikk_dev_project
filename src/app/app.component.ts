6309c0f6c2f6de14cd10f674ebd8edc3943446f0
Miguel Shinyenyi <miguel.amwayi.shinyenyi@gmail.com>
2022-11-10 22:57:26 +0300

    Add login and registration functionality with firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginAndRegistrationService } from './login-and-registration/login-and-registration.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quikk-dev-project';
+
+  constructor(public loginAndRegistrationService: LoginAndRegistrationService,
+    private router: Router
+    ) { }
+
+  logout() {
+    this.loginAndRegistrationService.logout().subscribe(() => {
+      this.router.navigate(['/loginAndRegistration']);
+    });
+  }
 }
